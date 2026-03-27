e6e4e1a18fcac320234859dfc5656f7d743ebe46
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2026-03-27 15:10:54 -0400

    added migration

---
+++ src/db/migration.js
@@ -0,0 +1,14 @@
+import sql from './connection.js';
+
+async function buildEmployees() {
+  await sql`CREATE TABLE IF NOT EXISTS employees (
+    employee_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    manager_id INTEGER CHECK (manager_id >=0),
+    income_type VARCHAR(20) DEFAULT 'wage',
+    income money,
+    updated_at TIMESTAMP DEFAULT NOW()
+  )`;
+}
+
+buildEmployees();
