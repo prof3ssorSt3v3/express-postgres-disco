b8636513bd4b1e46c5e3af960819f90f76f3ce0a
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2026-03-28 10:26:57 -0400

    add comments to buildEmployees function

--- src/db/migration.js
+++ src/db/migration.js
@@ -2,18 +2,21 @@ import sql from './connection.js';
 
 async function buildEmployees() {
   try {
+    //run your SQL command(s)
     await sql`CREATE TABLE IF NOT EXISTS employees (
-    employee_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    full_name VARCHAR(100) NOT NULL,
-    manager_id INTEGER CHECK (manager_id >=0),
-    income_type VARCHAR(20) DEFAULT 'wage',
-    income money,
-    updated_at TIMESTAMP DEFAULT NOW()
-  )`;
+      employee_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+      full_name VARCHAR(100) NOT NULL,
+      manager_id INTEGER CHECK (manager_id >=0),
+      income_type VARCHAR(20) DEFAULT 'wage',
+      income money,
+      updated_at TIMESTAMP DEFAULT NOW()
+    )`;
   } catch (err) {
+    //output an error message for the action log
     console.log(err.message);
   } finally {
-    await sql.end(); // Close the connection pool
+    await sql.end();
+    // Close the connection pool
   }
 }
 
